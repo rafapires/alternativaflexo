e13c0cf495a1bbf9ab29d7bb00195eab8c5222f1
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2017-02-10 12:05:50 -0200

    site atual parte final

---
+++ _head.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="Empresa de maquinas e produtos para area grafica.">
+<meta name="keywords" content="cliches, flexografia, lavadora de cliche, sistema de lavagem automatico,  laminas raspadoras, limpeza cliches, chapas de impressao, cyrel">
+<title><?php echo $title; ?></title>
+<meta name="google-site-verification" content="vtpO1QLY8U1B7fZPJ03iOM6OpZajvjKNXdmpsnHWnr8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="Máquinas, produtos e equipamentos para flexografia" />
+<meta name="author" content="Alternativa Flexo" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="plugins/flexslider/flexslider.css" rel="stylesheet" media="screen" />
+<!-- <link href="css/cubeportfolio.min.css" rel="stylesheet" />-->
+<link href="css/style.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<!-- Theme skin -->
+<link id="t-colors" href="skins/red.css" rel="stylesheet" />
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<style type="text/css">
+body,td,th {
+	font-family: "Open Sans", Arial, sans-serif;
+}
+h1,h2,h3,h4,h5,h6 {
+	font-family: Roboto, sans-serif;
+}
+</style>
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
---
+++ institucional.php
@@ -0,0 +1,88 @@
+<?php 
+$title = 'Conheça nossa empresa | Alternativa Flexo - Soluções para sistemas Flexográficos';
+include("_head.php"); ?>
+<style>
+.institucional {
+	text-decoration: underline;
+	font-weight: bold!important;
+}
+#content img {
+	max-width: 100%;
+	margin-top: 35px;
+}
+</style>
+<body>
+<div id="wrapper">
+  <?php include("_header.php"); ?>
+  <section id="inner-headline">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <ul class="breadcrumb">
+            <li><a href="index.php"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+            <li class="active">Institucional</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="content">
+    <div class="container">
+      <div class="col-md-9">
+        <div class="col-md-12">
+          <h3>Apresentação</h3>
+          <p> Com mais de 15 anos de experiência no mercado de impressão flexográfica, atualmente somos uma das maiores empresas do ramo no Brasil. Especializada na comercialização de máquinas, equipamentos, insumos e produtos para indústria de embalagem e impressão gráfica. Importamos dos Estados Unidos e Europa, os melhores produtos e a mais avançada tecnologia, para satisfação de nossos clientes. </p>
+          <p> Oferecemos soluções que agregam valor, reduzindo desperdícios, aumentando a vida útil, incrementando a produtividade e melhorando o ambiente de trabalho: </p>
+        </div>
+        <!-- Lists -->
+        <div class="row"> 
+          <!-- Unordered Lists -->
+          <div class="col-lg-6">
+            <h4>Produtos que reduzem o desperdício</h4>
+            <ul>
+              <li>Lavadora de clichês (menor consumo de solvente)</li>
+              <li>Lâminas Swed/Cut (melhor raspagem, reduzindo consumo de tinta)</li>
+              <li>Gravação direta a Laser (elimina os processos de exposição e revelação fotográfica)</li>
+            </ul>
+          </div>
+          <!-- Unstyled Lists -->
+          <div class="col-lg-6">
+            <h4>Produtos que aumentam a vida útil</h4>
+            <ul class="unstyled">
+              <li>Lavadora de clichês (elimina reposição de clichês por perda de pontos ou desgaste prematuro)</li>
+              <li>Laminas Swed/Cut (reduz o atrito com o anilox, aumentando sua durabilidade)</li>
+              <li> Mesa de corte de clichês (elimina perda de clichês por corte imprecisos e imperfeitos)</li>
+            </ul>
+          </div>
+          <!-- Ordered Lists -->
+          <div class="col-lg-6">
+            <h4>Produtos que incrementam a produtividade</h4>
+            <ul>
+              <li>Lavadora de Clichês (reduz o tempo de limpeza em 80%, liberando o funcionário para outras atividades)</li>
+              <li>Laminas Swed/Cut (maior durabilidade da lâmina garante menos trocas por mês)</li>
+              <li> Camisas gravadas a Laser (registro perfeito, menor setup, maior velocidade de impressão)</li>
+              <li> Mesa de corte de clichês (maior velocidade no refile do clichê após a gravação do clichê)</li>
+            </ul>
+          </div>
+          <!-- Unstyled Lists -->
+          <div class="col-lg-6">
+            <h4>Produtos que melhoram o ambiente de trabalho</h4>
+            <ul>
+              <li>Lavadora de clichês (elimina a exposiçao do funcionário a solvente e seus vapores)</li>
+              <li> Mesa de corte de clichês (elimina o risco de cortes e ferimentos causados pelo estilete)</li>
+            </ul>
+          </div>
+          <!-- Unstyled Lists -->
+          <div class="col-lg-12">
+            <h4>Produtos inovadores e exclusivos</h4>
+            <ul>
+              <li>Filme ablativo para gravadora digital (elimina ImageSetter)</li>
+              <li>Redutor de espessura de clichê (de 2.8mm para 1.7mm / de 1.7mm para 0,76mm)</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3"> <img src="img/apresentacao.jpg" alt="Apresentação Alternativa Flexo"/> </div>
+    </div>
+  </section>
+  <?php include("_footer.php"); ?>
