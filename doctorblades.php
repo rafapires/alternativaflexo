d7cdba10d212ada73498f0b0e86713e9eb4b1ff4
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2017-02-17 22:35:02 -0200

    hot site finalizado

---
+++ doctorblades.php
@@ -0,0 +1,63 @@
+<?php
+$title = 'Alternativa Flexo - Soluções para sistemas Flexográficos';
+include("_head.php"); ?>
+<style>
+.h2light {
+	font-weight:400;
+}
+
+#parallax1 .icon, #parallax1 h3 {
+	color:#fff;	
+}
+body {
+	background-color: #fcfcfc;
+}
+.home {
+	text-decoration:underline;
+	font-weight:bold!important;
+}
+@media (max-width: 480px) {
+  .btn-theme {
+	  font-size: 14px;	
+  }
+}
+</style>
+<body>
+<div id="wrapper"> 
+  <?php include("_header.php"); ?>
+
+    
+    
+    <!-- parallax  -->
+    <div id="parallax2" class="parallax2 text-light text-center marginbot50" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row appear">
+          <div class="col-sm-6 col-sm-offset-3">
+              <div class="box">
+                <div class="aligncenter">
+                  <iframe src="https://services.cognitoforms.com/f/7ceFa8gTwk-bPYBBx_5LAA?id=2" style="position:relative;width:1px;min-width:100%;*width:100%;" frameborder="0" scrolling="yes" seamless="seamless" height="300" width="100%" class="wufoo-form"></iframe>
+                  <script src="https://services.cognitoforms.com/scripts/embed.js"></script>
+                </div>
+              </div>
+          </div>
+            
+        </div>
+      </div>
+    </div>
+    
+    <!-- clients -->
+    <div class="container">
+    <div class="text-center">
+                <h2>Parcerias</h2>
+            </div>
+      <div class="row">
+        <div class="col-xs-6 col-md-2 aligncenter client"> <a href="http://www.agigraphics.it/it/" target="_blank" title="AGI Graphics"><img alt="AGI Graphics" src="img/clients/logo1.png" class="img-responsive" /></a> </div>
+        <div class="col-xs-6 col-md-2 aligncenter client"> <a href="http://swedev.se/" target="_blank" title="Swedev"><img alt="Swedev" src="img/clients/logo2.png" class="img-responsive" /></a> </div>
+        <div class="col-xs-6 col-md-2 aligncenter client"> <a href="http://www.flintgrp.com/" target="_blank" title="Flint Group"><img alt="Flint Group" src="img/clients/logo3.png" class="img-responsive" /></a> </div>
+        <div class="col-xs-6 col-md-2 aligncenter client"> <a href="http://www.leadlasers.com/" target="_blank" title="Lead Laser"><img alt="Lead Laser" src="img/clients/logo4.png" class="img-responsive" /></a> </div>
+        <div class="col-xs-6 col-md-2 aligncenter client"> <a href="http://www.keencut.com/" target="_blank" title="Keen Cut - Cutting Machines"><img alt="Keen Cut - Cutting Machines" src="img/clients/logo5.png" class="img-responsive" /></a> </div>
+        <div class="col-xs-6 col-md-2 aligncenter client"> <a href="http://www.pcicoatings.com/" target="_blank" title="PCI - Precision Coatings, Inc"><img alt="PCI - Precision Coatings, Inc" src="img/clients/logo6.png" class="img-responsive" /></a> </div>
+      </div>
+    </div>
+  </section>
+<?php include("_footer.php"); ?>
