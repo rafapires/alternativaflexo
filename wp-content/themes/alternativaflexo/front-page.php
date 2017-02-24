354c75edffd35a2ea399d0798e0b26c2bbfbe9b4
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2017-02-24 12:26:07 -0300

    acerta layout do carrocel da fornt-page

--- wp-content/themes/alternativaflexo/front-page.php
+++ wp-content/themes/alternativaflexo/front-page.php
@@ -4,18 +4,11 @@
 
 
 <div id="carousel-frontpage" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-frontpage" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-frontpage" data-slide-to="1"></li>
-    <li data-target="#carousel-frontpage" data-slide-to="2"></li>
-    <li data-target="#carousel-frontpage" data-slide-to="3"></li>
-  </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-		<img src="<?php echo get_template_directory_uri(); ?>/img/slides/flexslider/banner4.jpg" alt="" />
+		<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/slides/flexslider/banner4.jpg" alt="" />
       	<div class="carousel-caption">
 			<h3>lavadora de Clichês - ACQUASOLVE</h3>
             <p>Soluções econômicas de alta qualidade</p>
@@ -23,7 +16,7 @@
       </div>
     </div>
     <div class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/slides/flexslider/banner2.jpg" alt="">
+      <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/slides/flexslider/banner2.jpg" alt="">
       <div class="carousel-caption">
   			<h3>SWED/CUT® Premium Doctor Blades</h3>
             <p>Aço e rebaixo totalmente produzido na suécia</p>
@@ -31,7 +24,7 @@
       </div>
     </div>
     <div class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/slides/flexslider/banner3.jpg" alt="">
+      <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/slides/flexslider/banner3.jpg" alt="">
       <div class="carousel-caption">
   			<h3>DEMOUNTER</h3>
             <p>Sistema automático para descolagem de clichês</p>
@@ -39,7 +32,7 @@
       </div>
     </div>
     <div class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/slides/flexslider/banner5.jpg" alt="">
+      <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/slides/flexslider/banner5.jpg" alt="">
       <div class="carousel-caption">
   			<h3>Lavadora de Anilox ECOCLEAN</h3>
             <p>Equipamento Automatizado 100% nacional. Desenvolvidas pela Flexocom, e fabricadas por nossa equipe</p>
@@ -49,15 +42,15 @@
     ...
   </div>
 
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-frontpage" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-frontpage" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-frontpage" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-frontpage" data-slide-to="1"></li>
+    <li data-target="#carousel-frontpage" data-slide-to="2"></li>
+    <li data-target="#carousel-frontpage" data-slide-to="3"></li>
+  </ol>
+
+
 </div>
 
 
