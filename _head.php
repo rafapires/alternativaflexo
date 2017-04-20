d26620ddb8f4aee4c7331e13f39040820a858a3d
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2017-04-20 14:05:40 -0300

    coloca código de rastreio do Google Analytics

--- _head.php
+++ _head.php
@@ -34,6 +34,18 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92271054-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
 <style type="text/css">
 body,td,th {
 	font-family: "Open Sans", Arial, sans-serif;
